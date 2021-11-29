382120188e681c8328a9dce846b6717cee817f4f
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-29 18:53:45 +0800

    Delete lib directory

--- lib/verify_urls.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require 'url_finder'
-require 'verify_urls/version'
-require 'verify_urls/helper'
-
-module VerifyUrls
-end
--- lib/verify_urls/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module VerifyUrls
-  VERSION = '0.2.0'.freeze
-end
